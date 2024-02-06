ea388357219a012af6028f4264c736da2cce6ebc
AnthonyMizal <202011040@gordoncollege.edu.ph>
2024-02-06 18:06:45 +0800

    add task on projects tab

--- cypress/e2e/projects/addTaskOnProjects.spec.cy.js
+++ cypress/e2e/projects/addTaskOnProjects.spec.cy.js
@@ -9,10 +9,135 @@ describe('Add Task on Projects Page', () => {
         login()
         goToProjectsPage()
     })
+    
     it('should be able to see Projects Heading', () => {
         const dashboardElement = cy.get('h2.text-xl.font-semibold')
         dashboardElement.should('contain', 'Projects')
     })
+    it('should be able to add task and description then delete', () => {
+        const projectTask = cy.get('div.flex.flex-col.w-full')
+        projectTask.within(() => {
+            cy.contains('Stephen Juridico').click()
+        })
+        const addTaskButton = cy.contains('Add Task')
+        addTaskButton.click()
+
+        const inputTaskField = cy.get('#newTaskField')
+        inputTaskField.type('Anthony Task')
+
+        const addButton = cy.contains('Add')
+        addButton.click()
+
+        const taskDescriptionField = cy.get('textarea.ring-2')
+        taskDescriptionField.type('Sample Task')
+
+        const taskDescriptionCheckButton = cy.get('div.absolute.bottom-2.right-2.space-x-1')
+        taskDescriptionCheckButton.within(() => {
+            cy.get('button:nth-child(2)').click()
+        })
+
+        const projectDeleteButton = cy.get('div.w-full.col-span-full')
+        projectDeleteButton.within(() => {
+            cy.get('button').click()
+        })
+
+        const deleteConfirmation = cy.get('div.flex.justify-center.items-center.space-x-4')
+        deleteConfirmation.within(() => {
+            cy.get('button:nth-child(2)').click()
+        })
+
+        
+    })
+
+    it('should be able to add task, description, and review notes then delete', () => {
+        const projectTask = cy.get('div.flex.flex-col.w-full')
+        projectTask.within(() => {
+            cy.contains('Stephen Juridico').click()
+        })
+        const addTaskButton = cy.contains('Add Task')
+        addTaskButton.click()
+
+        const inputTaskField = cy.get('#newTaskField')
+        inputTaskField.type('Anthony Task')
+
+        const addButton = cy.contains('Add')
+        addButton.click()
+
+        const taskDescriptionField = cy.get('textarea.ring-2')
+        taskDescriptionField.type('Sample Task')
+
+        const taskDescriptionCheckButton = cy.get('div.absolute.bottom-2.right-2.space-x-1')
+        taskDescriptionCheckButton.within(() => {
+            cy.get('button:nth-child(2)').click()
+        })
+
+        const addNewNoteButton = cy.get('div.flex.flex-col.w-full > ul > li > div > button')
+        addNewNoteButton.click()
+
+        const reviewNoteField = cy.get('div.flex.flex-col.w-full > ul > li > div > input')
+        reviewNoteField.type('Sample Review')
+
+        const saveReviewNote = cy.get('div.flex.flex-col.w-full > ul > li > div > div > button:nth-child(1)')
+        saveReviewNote.click()
+
+        const projectDeleteButton = cy.get('div.w-full.col-span-full')
+        projectDeleteButton.within(() => {
+            cy.get('button').click()
+        })
+
+        const deleteConfirmation = cy.get('div.flex.justify-center.items-center.space-x-4')
+        deleteConfirmation.within(() => {
+            cy.get('button:nth-child(2)').click()
+        })
+    })
 
 
+    it('should be able to add task, description, review notes, and add comment then delete', () => {
+        const projectTask = cy.get('div.flex.flex-col.w-full')
+        projectTask.within(() => {
+            cy.contains('Stephen Juridico').click()
+        })
+        const addTaskButton = cy.contains('Add Task')
+        addTaskButton.click()
+
+        const inputTaskField = cy.get('#newTaskField')
+        inputTaskField.type('Anthony Task')
+
+        const addButton = cy.contains('Add')
+        addButton.click()
+
+        const taskDescriptionField = cy.get('textarea.ring-2')
+        taskDescriptionField.type('Sample Task')
+
+        const taskDescriptionCheckButton = cy.get('div.absolute.bottom-2.right-2.space-x-1')
+        taskDescriptionCheckButton.within(() => {
+            cy.get('button:nth-child(2)').click()
+        })
+
+        const addNewNoteButton = cy.get('div.flex.flex-col.w-full > ul > li > div > button')
+        addNewNoteButton.click()
+
+        const reviewNoteField = cy.get('div.flex.flex-col.w-full > ul > li > div > input')
+        reviewNoteField.type('Sample Review')
+
+        const saveReviewNote = cy.get('div.flex.flex-col.w-full > ul > li > div > div > button:nth-child(1)')
+        saveReviewNote.click()
+
+        const commentField = cy.get('div.ql-editor:last')
+        commentField.click()
+        commentField.type('Sample Comment')
+
+        const commentButton = cy.contains("button", "COMMENT")
+        commentButton.click()
+
+        const projectDeleteButton = cy.get('div.w-full.col-span-full')
+        projectDeleteButton.within(() => {
+            cy.get('button').click()
+        })
+
+        const deleteConfirmation = cy.get('div.flex.justify-center.items-center.space-x-4')
+        deleteConfirmation.within(() => {
+            cy.get('button:nth-child(2)').click()
+        })
+    })
 })
