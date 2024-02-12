2e302087592876a726aa3f2e7394857067e18f89
AnthonyMizal <202011040@gordoncollege.edu.ph>
2024-02-12 17:00:10 +0800

    task2

--- cypress/e2e/projects/addTaskOnProjects.spec.cy.js
+++ cypress/e2e/projects/addTaskOnProjects.spec.cy.js
@@ -1,7 +1,7 @@
 import {login} from '../../utils/helpers/login'
 import {goToProjectsPage} from '../../utils/helpers/goToProjectsPage'
 import { goToHomePage } from '../../utils/helpers/goToHomePage'
-import { newtaskFieldTitle, taskDescriptionTitle, projectTitleHeader } from './createTaskConstants'
+import { newtaskFieldTitle, taskDescriptionTitle, projectTitleHeader, reviewNoteContent, commentContent } from './createTaskConstants'
 
 describe('Add Task on Projects Page', () => {
 
@@ -15,7 +15,7 @@ describe('Add Task on Projects Page', () => {
         const dashboardElement = cy.get('h2.text-xl.font-semibold')
         dashboardElement.should('contain', 'Projects')
     })
-    it.only('should be able to add task and description then delete', () => {
+    it('should be able to add task and description then delete', () => {
         const projectTask = cy.get('div.flex.flex-col.w-full')
         projectTask.within(() => {
             cy.contains('Anthony Corp').click()
@@ -68,31 +68,50 @@ describe('Add Task on Projects Page', () => {
         projectTask.within(() => {
             cy.contains('Anthony Corp').click()
         })
+        const projectTitleGet = cy.get('h1.text-2xl.font-semibold')
+        projectTitleGet.should('contain', projectTitleHeader)
+
+        // Create Task
         const addTaskButton = cy.contains('Add Task')
+        addTaskButton.should('exist')
         addTaskButton.click()
 
         const inputTaskField = cy.get('#newTaskField')
-        inputTaskField.type('Anthony Task')
+        inputTaskField.type(newtaskFieldTitle)
+
+        inputTaskField.invoke('val').should('contain', newtaskFieldTitle)
 
         const addButton = cy.contains('Add')
+        addButton.should('exist')
         addButton.click()
 
+        const newTaskTitle = cy.contains('textarea', newtaskFieldTitle)
+        newTaskTitle.should('exist')
+
+        // Add Task Description
         const taskDescriptionField = cy.get('textarea.ring-2')
-        taskDescriptionField.type('Sample Task')
+        taskDescriptionField.type(taskDescriptionTitle)
+
+        taskDescriptionField.invoke('val').should('eq', taskDescriptionTitle)
 
         const taskDescriptionCheckButton = cy.get('div.absolute.bottom-2.right-2.space-x-1')
         taskDescriptionCheckButton.within(() => {
             cy.get('button:nth-child(2)').click()
         })
 
+        // Add Note
         const addNewNoteButton = cy.get('div.flex.flex-col.w-full > ul > li > div > button')
         addNewNoteButton.click()
 
         const reviewNoteField = cy.get('div.flex.flex-col.w-full > ul > li > div > input')
-        reviewNoteField.type('Sample Review')
+        reviewNoteField.type(reviewNoteContent)
 
         const saveReviewNote = cy.get('div.flex.flex-col.w-full > ul > li > div > div > button:nth-child(1)')
         saveReviewNote.click()
+
+        const reviewNoteContentVal = cy.get('ul.w-full.flex.flex-col > li > div > input')
+        reviewNoteContentVal.invoke('val').should('eq', reviewNoteContent)
+    
 
         const projectDeleteButton = cy.get('div.w-full.col-span-full')
         projectDeleteButton.within(() => {
@@ -111,38 +130,62 @@ describe('Add Task on Projects Page', () => {
         projectTask.within(() => {
             cy.contains('Anthony Corp').click()
         })
+        const projectTitleGet = cy.get('h1.text-2xl.font-semibold')
+        projectTitleGet.should('contain', projectTitleHeader)
+
+        // Create Task
         const addTaskButton = cy.contains('Add Task')
+        addTaskButton.should('exist')
         addTaskButton.click()
 
         const inputTaskField = cy.get('#newTaskField')
-        inputTaskField.type('Anthony Task')
+        inputTaskField.type(newtaskFieldTitle)
+
+        inputTaskField.invoke('val').should('contain', newtaskFieldTitle)
 
         const addButton = cy.contains('Add')
+        addButton.should('exist')
         addButton.click()
 
+        const newTaskTitle = cy.contains('textarea', newtaskFieldTitle)
+        newTaskTitle.should('exist')
+
+        // Add Task Description
         const taskDescriptionField = cy.get('textarea.ring-2')
-        taskDescriptionField.type('Sample Task')
+        taskDescriptionField.type(taskDescriptionTitle)
+
+        taskDescriptionField.invoke('val').should('eq', taskDescriptionTitle)
 
         const taskDescriptionCheckButton = cy.get('div.absolute.bottom-2.right-2.space-x-1')
         taskDescriptionCheckButton.within(() => {
             cy.get('button:nth-child(2)').click()
         })
 
+        // Add Note
         const addNewNoteButton = cy.get('div.flex.flex-col.w-full > ul > li > div > button')
         addNewNoteButton.click()
 
         const reviewNoteField = cy.get('div.flex.flex-col.w-full > ul > li > div > input')
-        reviewNoteField.type('Sample Review')
+        reviewNoteField.type(reviewNoteContent)
 
         const saveReviewNote = cy.get('div.flex.flex-col.w-full > ul > li > div > div > button:nth-child(1)')
         saveReviewNote.click()
 
+        const reviewNoteContentVal = cy.get('ul.w-full.flex.flex-col > li > div > input')
+        reviewNoteContentVal.invoke('val').should('eq', reviewNoteContent)
+
+        // Add Comment
         const commentField = cy.get('div.ql-editor:last')
         commentField.click()
-        commentField.type('Sample Comment')
+        commentField.type(commentContent)
 
         const commentButton = cy.contains("button", "COMMENT")
+        commentButton.should('exist')
         commentButton.click()
+        const commentDiv = cy.get('div.flex.flex-col.space-y-4.mb-4')
+        commentDiv.within(() => {
+            cy.get('p').should('contain', commentContent)
+        })
 
         const projectDeleteButton = cy.get('div.w-full.col-span-full')
         projectDeleteButton.within(() => {
@@ -161,40 +204,65 @@ describe('Add Task on Projects Page', () => {
         projectTask.within(() => {
             cy.contains('Anthony Corp').click()
         })
+        const projectTitleGet = cy.get('h1.text-2xl.font-semibold')
+        projectTitleGet.should('contain', projectTitleHeader)
+
+        // Create Task
         const addTaskButton = cy.contains('Add Task')
+        addTaskButton.should('exist')
         addTaskButton.click()
 
         const inputTaskField = cy.get('#newTaskField')
-        inputTaskField.type('Anthony Task')
+        inputTaskField.type(newtaskFieldTitle)
+
+        inputTaskField.invoke('val').should('contain', newtaskFieldTitle)
 
         const addButton = cy.contains('Add')
+        addButton.should('exist')
         addButton.click()
 
+        const newTaskTitle = cy.contains('textarea', newtaskFieldTitle)
+        newTaskTitle.should('exist')
+
+        // Add Task Description
         const taskDescriptionField = cy.get('textarea.ring-2')
-        taskDescriptionField.type('Sample Task')
+        taskDescriptionField.type(taskDescriptionTitle)
+
+        taskDescriptionField.invoke('val').should('eq', taskDescriptionTitle)
 
         const taskDescriptionCheckButton = cy.get('div.absolute.bottom-2.right-2.space-x-1')
         taskDescriptionCheckButton.within(() => {
             cy.get('button:nth-child(2)').click()
         })
 
+        // Add Note
         const addNewNoteButton = cy.get('div.flex.flex-col.w-full > ul > li > div > button')
         addNewNoteButton.click()
 
         const reviewNoteField = cy.get('div.flex.flex-col.w-full > ul > li > div > input')
-        reviewNoteField.type('Sample Review')
+        reviewNoteField.type(reviewNoteContent)
 
         const saveReviewNote = cy.get('div.flex.flex-col.w-full > ul > li > div > div > button:nth-child(1)')
         saveReviewNote.click()
 
+        const reviewNoteContentVal = cy.get('ul.w-full.flex.flex-col > li > div > input')
+        reviewNoteContentVal.invoke('val').should('eq', reviewNoteContent)
+
+        // Add Comment
         const commentField = cy.get('div.ql-editor:last')
         commentField.click()
-        commentField.type('Sample Comment')
+        commentField.type(commentContent)
 
         const commentButton = cy.contains("button", "COMMENT")
+        commentButton.should('exist')
         commentButton.click()
+        const commentDiv = cy.get('div.flex.flex-col.space-y-4.mb-4')
+        commentDiv.within(() => {
+            cy.get('p').should('contain', commentContent)
+        })
 
         const dateButton = cy.contains("button", "Set Due Date")
+        dateButton.should('exist')
         dateButton.click()
 
         const dateContainer = cy.get("div.absolute.z-50.block.pt-2.top-12.date-picker-container")
@@ -203,6 +271,9 @@ describe('Add Task on Projects Page', () => {
                 cy.get('span:nth-child(12)').click()
             })
         })
+
+        const dateValueCont = cy.get('div.relative.w-full.bg-transparent > div > div > div  > button > span')
+        dateValueCont.invoke('text').should('eq', '08-Feb-2024')
 
         const projectDeleteButton = cy.get('div.w-full.col-span-full')
         projectDeleteButton.within(() => {
